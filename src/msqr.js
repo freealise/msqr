cac6f1369cbd29aeaf4429f6c7b2c384555027ab
Marina Goritskaia <5275747+freealise@users.noreply.github.com>
2025-03-22 20:26:53 +0300

    Update msqr.js

--- src/msqr.js
+++ src/msqr.js
@@ -125,11 +125,11 @@ function MSQR(src, options) {
 				
 				imgData = ctx.getImageData(0, 0, c.width, c.height);
 				for (let i = 0; i < imgData.data.length; i += 4) {
-			   if (imgData.data[i] == 0 && imgData.data[i+1] == 0 && imgData.data[i+2] == 0 && holes[i/4] === true) {
+			   if (holes[i/4] === true) {
 							 imgData.data[i] = 255;
-								imgData.data[i+1] = 0;
-								imgData.data[i+2] = 255;
-								imgData.data[i+3] = 127;
+								imgData.data[i+1] = 255;
+								imgData.data[i+2] = 0;
+								imgData.data[i+3] = 255;
 				  }
 		  }
 				ctx.putImageData(imgData, 0, 0);
